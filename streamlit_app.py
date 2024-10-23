f2a7b155836725ac4c5776b5a88e2fb555aca934
Hkmhqddst16 <hikmahq16@gmail.com>
2024-10-23 10:06:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,80 +1,47 @@
 import streamlit as st
 import pandas as pd
-from bertopic import BERTopic
-import matplotlib.pyplot as plt
 from wordcloud import WordCloud
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-import string
+import matplotlib.pyplot as plt
+import re
 
-# Judul aplikasi
-st.title("BERTopic dengan Streamlit")
+st.set_page_config(page_title="BERTopic", page_icon="🤗")
 
-# Deskripsi
-st.write("Ini adalah contoh sederhana menggunakan Streamlit dan BERTopic.")
+df = pd.read_csv("bertopic_results.csv")
+st.title("Topic modeling using BERTopic")
 
+st.markdown("Mengambil 15 topik dan berikut 10 contoh dari isi topik :")
+st.dataframe(df.sample(10))
 
-df = pd.read_csv('../display_data.csv')
+st.subheader("Distribution of topics in the collection of articles.")
+topic_distribution = df['topic'].value_counts().sort_index()
+st.bar_chart(topic_distribution)
 
-def preprocess_text(text):
-    # Convert text to lowercase
-    text = text.lower()
+def contnet_topwords(topic):
+    topic_df = df[df['topic'] == topic]
+    text = ' '.join(topic_df['content'])
+    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
+    plt.figure(figsize=(10, 5))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    st.pyplot(plt)
+    
 
-    # Remove punctuation
-    text = text.translate(str.maketrans('', '', string.punctuation))
+st.subheader('kata teratas dan WordCloud dari topik')
+topic_selected = st.selectbox('Select a Topic', sorted(df['topic'].unique()))
 
-    # Tokenize text
-    tokens = word_tokenize(text)
+topic_df = df[df['topic'] == topic_selected]
+all_content = ' '.join(topic_df['content'])
 
-    # Remove stop words
-    stop_words = set(stopwords.words('english'))
-    tokens = [word for word in tokens if word not in stop_words]
+words = re.findall(r'\b\w+\b', all_content.lower())
 
-    # Join tokens back to string
-    preprocessed_text = ' '.join(tokens)
+word_counts = pd.Series(words).value_counts()
+top_words = word_counts.head(10)
+st.write(f"kata teratas dari topik {topic_selected}")
+st.write(top_words)
 
-    return preprocessed_text
+top_articles = topic_df.head(10)  
+st.write(f"Top 10 Topik {topic_selected}")
+st.write(top_articles[['content', 'topic']])
 
-# Apply preprocessing to the content column
-df['content'] = df['content'].apply(preprocess_text)
-# Extract the content column as a list of documents
-documents = df['content'].tolist()
-# Initialize BERTopic with a maximum of 15 topics
-topic_model = BERTopic(nr_topics=15)
-# Fit the model to your documents
-topics, probabilities = topic_model.fit_transform(documents)
-topics_adjusted = [topic + 1 if topic > -1 else 0 for topic in df['topic']]
-df['topic'] = topics_adjusted
-print(df)
-
-df = df.drop(['category'], axis = 1 )
-df.to_csv("bertopic_results.csv", index = False)
-
-# Visualizing top 10 words for a particular topic, e.g., topic 0
-topic_number = 5
-top_n_words = 10
-
-# Get the words and their scores for the selected topic
-topic_words = topic_model.get_topic(topic_number)[:top_n_words]
-
-# Extract words and their scores
-words = [word for word, _ in topic_words]
-scores = [score for _, score in topic_words]
-
-# Plotting the top words
-plt.figure(figsize=(10, 6))
-plt.barh(words, scores, color='skyblue')
-plt.xlabel('Scores')
-plt.ylabel('Words')
-plt.title(f'Top {top_n_words} words in Topic {topic_number}')
-plt.gca().invert_yaxis()
-plt.show()
-
-# Generate and display a word cloud for the selected topic
-wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(dict(topic_words))
-
-plt.figure(figsize=(10, 6))
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis('off')
-plt.title(f'Word Cloud for Topic {topic_number}')
-plt.show()
+st.write(f"Word Cloud topik {topic_selected}")
+contnet_topwords(topic_selected)
